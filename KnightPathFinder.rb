ed2f0c6cf66c87bd15b692c7a7fccf0f7c29de92
william_brooks_miller <brooksmiller.miller@gmail.com>
2020-02-01 15:53:50 -0500

    Knight's Travails project at completion of Part 1

---
+++ KnightPathFinder.rb
@@ -0,0 +1,61 @@
+require_relative "polytreenode"
+
+class KnightPathFinder
+    MOVES = [[2,1],[2,-1],[-2,1],[-2,-1],[1,2],[1,-2],[-1,2],[-1,-2]].freeze
+
+    attr_reader :root_node, :considered_positions
+
+    def initialize(pos)
+        valid_pos?(pos)
+        @root_node = PolyTreeNode.new(pos)
+        @considered_positions = [pos]
+        build_move_tree
+    end
+    
+    def new_move_positions(pos)
+        valid_pos?(pos)
+        new_moves = Array.new()
+        posible_moves = valid_moves(pos)
+        posible_moves.each do |move|
+            if @considered_positions.include?(move) == false
+                new_moves << move
+                @considered_positions << move
+            end
+        end
+        new_moves
+    end
+
+    def build_move_tree
+        root_node = PolyTreeNode.new(@root_node.value)
+        
+        # build tree out in bfs style
+        nodes = [root_node]
+        until nodes.empty?
+            curr_node = nodes.shift
+            curr_pos = curr_node.value
+            new_move_positions(curr_pos).each do |next_pos|
+                next_node = PolyTreeNode.new(next_pos)
+                curr_node.add_child(next_node)
+                nodes << next_node
+            end
+        end
+    end
+    
+    private
+    
+    def valid_pos?(pos)
+        raise error if pos.is_a?(Array) == false || pos.length != 2
+        pos.each{ |ele| raise error if ele < 0 || ele > 7 }
+    end
+    
+    def valid_moves(pos)
+        valid_pos?(pos)
+        moves = Array.new()
+        MOVES.each do |move|
+            new_x = pos.first + move.first
+            new_y = pos.last + move.last
+            moves << [new_x,new_y] if new_x.between?(0,7) && new_y.between?(0,7)
+        end
+        moves
+    end
+end
---
+++ PolyTreeNode.rb
@@ -0,0 +1,49 @@
+class PolyTreeNode
+
+    attr_reader :parent, :children, :value
+
+    def initialize(value)
+        @parent = nil
+        @children = Array.new()
+        @value = value
+    end
+
+    def parent= (node)
+        if @parent != nil && node != nil
+            @parent.children.delete(self)
+        end
+        @parent = node
+        node.children << self if node != nil
+    end
+
+    def remove_child(node)
+        raise error if @children.include?(node) == false
+        node.parent=(nil)
+    end
+
+    def add_child(node)
+        if @children.include?(node) == false
+            node.parent=(self)
+        end
+    end
+
+    def dfs(target)
+        return self if target == self.value
+        @children.each do |child|
+            search_result = child.dfs(target)
+            return search_result unless search_result.nil?
+        end
+        nil
+    end
+
+    def bfs(target)
+        queue = Array.new()
+        queue << self
+        until queue.empty?
+            el = queue.shift
+            return el if target == el.value
+            el.children.each{ |child| queue << child}
+        end
+    end
+
+end
